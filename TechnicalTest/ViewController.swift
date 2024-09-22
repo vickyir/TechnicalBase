8b97e4bbb34d36936c6ef6d5290b19d6307dfe4d
vicky.metrocom <vicky.irwanto@metrocomjaddi.com>
2024-09-22 19:53:30 +0700

    Initial Commit

---
+++ TechnicalTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechnicalTest
+//
+//  Created by Vicky Irwanto on 22/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
